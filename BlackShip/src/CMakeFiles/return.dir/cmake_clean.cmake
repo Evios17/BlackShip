b741ff0120cd915249e83f8c726a664868f346f5
Evios17 <antoine@outlook.com>
2022-11-05 17:57:00 +0100

    ok

---
+++ BlackShip/src/CMakeFiles/return.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../build/return"
+  "../build/return.pdb"
+  "CMakeFiles/return.dir/return.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/return.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
