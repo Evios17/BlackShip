221d0f5a768ba95137932c91574a3b69f8589527
Evios17 <antoine@outlook.com>
2022-11-07 06:33:01 +0100

    BUG FIXE 2

--- BlackShip/src/network.c
+++ BlackShip/src/network.c
@@ -1,30 +1,57 @@
-#include <network.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
+/*
+ * Find local ip used as source ip in ip packets.
+ * Use getsockname and a udp connection
+ */
 
-void ip () {
-    FILE *file;
-    int c;
+#include<stdio.h>	//printf
+#include<string.h>	//memset
+#include<errno.h>	//errno
+#include<sys/socket.h>	//socket
+#include<netinet/in.h> //sockaddr_in
+#include<arpa/inet.h>	//getsockname
+#include<unistd.h>	//close
 
-    system("echo $(hostname -I | awk '{print $1}') > ip.tmp");
-    file = fopen("ip.tmp", "r");
-
-    while ((c = fgetc(file)) != EOF)
+int ipaddress ()
+{
+    char command[50];
+    const char* google_dns_server = "8.8.8.8";
+    int dns_port = 53;
+	
+	struct sockaddr_in serv;
+    
+    int sock = socket ( AF_INET, SOCK_DGRAM, 0);
+    
+    //Socket could not be created
+    if(sock < 0)
     {
-        printf("%c", c);
-    }
-    fclose(file);
-    remove("ip.tmp");    
-}
+		perror("Socket error");
+	}
+    
+	memset( &serv, 0, sizeof(serv) );
+    serv.sin_family = AF_INET;
+    serv.sin_addr.s_addr = inet_addr( google_dns_server );
+    serv.sin_port = htons( dns_port );
 
-int chk_w () {
-    FILE *chk;
-    chk = fopen ("check", "w");
-    remove("check");
-    if (chk == NULL) {
-        return false;
-    } else {
-        return true;
-    }
+    int err = connect( sock , (const struct sockaddr*) &serv , sizeof(serv) );
+    
+    struct sockaddr_in name;
+    socklen_t namelen = sizeof(name);
+    err = getsockname(sock, (struct sockaddr*) &name, &namelen);
+    	
+	char buffer[100];
+    const char* p = inet_ntop(AF_INET, &name.sin_addr, buffer, 100);
+    	
+	if(p != NULL)
+	{
+		printf("Votre adresse IP à partager est : %s \n" , buffer);
+	}
+	else
+	{
+		//Some error
+		printf ("Error number : %d . Error message : %s \n" , errno , strerror(errno));
+	}
+
+    close(sock);
+
+    return 0;
 }
